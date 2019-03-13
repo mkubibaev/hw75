9109c3503168cede3e2414dee4a14d498fe70da1
mkubibaev <mkubibaev@gmail.com>
2019-03-14 01:12:04 +0600

    added form template

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+    state = {
+
+    };
+
     render() {
         return (
-            <div className="App">
+            <div className="container">
+                <textarea placeholder="message for encode"/>
+                <input type="text" placeholder="password"/>
+                <button type="button">Encode</button>
+                <button type="decode">Decode</button>
+                <textarea placeholder="message for decode"/>
             </div>
         );
     }
